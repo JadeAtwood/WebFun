7af3aa76ed14036f2d269343b86785c3a8020c0e
Jade Atwood <jadeatwood@protonmail.com>
2021-06-28 20:16:50 -0400

    complete, no bonus

---
+++ makingConnections/script.js
@@ -0,0 +1,9 @@
+function nameChanger(Element) {
+    document.querySelector("#nombre").innerText="Dog Barker";
+}
+
+function hide(Element) {
+    document.querySelector(".requestDetails").remove();
+}
+
+
