5b33ed028e914eeed3080d9e0a3fc38da7868e8e
Jade Atwood <jadeatwood@protonmail.com>
2021-06-29 13:44:20 -0400

    manditory done, bonus is still incomplete

---
+++ dojoWeather/script.js
@@ -0,0 +1,17 @@
+function dontGo(element) {
+    alert("PLEASE, DONT GO !!!!!!!!")
+}
+
+function hide() {
+    var popup = document.getElementById('popUp');
+    popup.remove();
+}
+
+// function change(){
+//     document.getElementById('high1');
+//     if (high1 != ) {
+//         document.getElementById('high1').innerText='74';
+//         console.log('gooble gobble')
+//     }
+    
+// }
